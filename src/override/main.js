013888403afff12b020bb6ed5f5958cd4607f90f
Franklin Zhang <franklinzgma@gmail.com>
2019-03-02 04:50:45 -0600

    added buttons and basic layout TODO: reconfigure layout and fix overlappinng buttons

--- src/override/main.js
+++ src/override/main.js
@@ -1,9 +1,59 @@
-function openSettings () {
+var userName;
 
-    var x = document.getElementById("settings");
-    if (x.style.display === "none") {
-    x.style.display = "block";
-    } else {
-    x.style.display = "none";
-    }
+function openSettings () {
+	document.getElementById('settings').classList.toggle("settings-open");
+	clearName();
 }
+
+function saveName() {
+	localStorage.setItem('receivedName', userName);
+
+	userName=localStorage.getItem('receivedName');
+
+	if( userName == null){
+		userName = "friend";
+	}
+}
+
+function changeName() {
+	if(userName = document.getElementById("name-input").value == ""){
+		userName = "friend";
+	}else {
+		userName = document.getElementById("name-input").value;
+	}
+	
+
+	saveName();
+	getGreeting();
+}
+
+function getGreeting() {
+	document.getElementById('greeting').innerHTML = `Hello, ${userName}. What news are you interested in today?`;
+}
+
+function clearName() {
+	document.getElementById("name-input").value = "";
+}
+
+document.getElementById("settings-button").addEventListener('click', openSettings);
+
+document.getElementById("name-form").addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	changeName();
+	clearName();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var btn = document.querySelector('.button'),
+        loader = document.querySelector('.loader'),
+        check = document.querySelector('.check');
+    
+    btn.addEventListener('click', function () {
+      loader.classList.add('active');    
+    });
+   
+    loader.addEventListener('animationend', function() {
+      check.classList.add('active'); 
+    });
+  });
